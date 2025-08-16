38b8d8c5b630f717d5941637fdbb3ba989e71f6c
Victor Rodriguez <thevictor795@gmail.com>
2025-08-15 19:51:22 -0700

    Added a home page, added navigation from the sign up to home page

--- lib/views/pages/sign_up.dart
+++ lib/views/pages/sign_up.dart
@@ -1,3 +1,4 @@
+import 'package:ecg_app/views/pages/home.dart';
 import 'package:flutter/material.dart';
 
 class SignUpPage extends StatefulWidget {
@@ -15,7 +16,24 @@ class _SignUpPageState extends State<SignUpPage> {
         title: Text("Sign up", selectionColor: Color(0xFF1D1B14)),
         backgroundColor: Color(0xFF07A0C3),
       ),
-      body: Center(child: Text("Hi you are in the sign up page")),
+      body: Column(
+        children: [
+          Text("Hi you are in the sign up page"),
+          Center(
+            child: FilledButton(
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return HomePage();
+                  },
+                ),
+              ),
+              child: Text("Go to home"),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }

c7518231c0604abdba756f59b9cdc184f3156e82
Victor Rodriguez <thevictor795@gmail.com>
2025-08-21 18:40:15 -0700

    Removed duplicate logic from ble_device_status.dart

--- lib/views/widgets/ble_device_status.dart
+++ lib/views/widgets/ble_device_status.dart
@@ -1,4 +1,3 @@
-import 'package:ecg_app/views/widgets/ble_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:ecg_app/data/classes/notifiers.dart';
 
@@ -15,35 +14,24 @@ class EcgStatusWidget extends StatelessWidget {
         return Material(
           color: isConnected ? Colors.tealAccent : Colors.grey.shade300,
 
-          child: InkWell(
-            onTap: isConnected
-                ? () {
-                    connectedDevice.value = null;
-                    BleEcgManager().disconnect();
-                    selectedPageNotifier.value = 1;
-                  }
-                : null,
-            splashColor: Colors.blue,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                style: TextStyle(color: Colors.red, fontSize: 14),
+                isConnected
+                    ? device.device.platformName
+                    : "No connected device",
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(width: 2),
 
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  style: TextStyle(color: Colors.red, fontSize: 14),
-                  isConnected
-                      ? device.device.platformName
-                      : "No connected device",
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(width: 2),
-
-                Icon(
-                  Icons.circle,
-                  color: isConnected ? Colors.green : Colors.red,
-                  size: 14,
-                ),
-              ],
-            ),
+              Icon(
+                Icons.circle,
+                color: isConnected ? Colors.green : Colors.red,
+                size: 14,
+              ),
+            ],
           ),
         );
       },

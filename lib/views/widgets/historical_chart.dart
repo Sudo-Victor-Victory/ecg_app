9d2282e5090ddb863d0a11038154382f6d6d86ab
Victor Rodriguez <thevictor795@gmail.com>
2025-09-10 19:33:59 -0700

    Added smooth sliding by utilizing onRendererCreated, onSelectionChanged, onPlotAreaSwiped, and updated packages

--- lib/views/widgets/historical_chart.dart
+++ lib/views/widgets/historical_chart.dart
@@ -2,9 +2,10 @@ import 'package:ecg_app/views/pages/ecg_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
-class HistoricalChart extends StatelessWidget {
+class HistoricalChart extends StatefulWidget {
   final List<Map<String, dynamic>> ecgRows;
   final DateTime startTime;
+
   const HistoricalChart({
     super.key,
     required this.ecgRows,
@@ -12,10 +13,29 @@ class HistoricalChart extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
+  State<HistoricalChart> createState() => _HistoricalChartState();
+}
+
+class _HistoricalChartState extends State<HistoricalChart> {
+  late List<EcgDataPoint> chartData;
+  // Both of these vars act as the left and right bound of the chart.
+  late double axisVisibleMin;
+  late double axisVisibleMax;
+  late SelectionBehavior selectionBehavior;
+  // Controller that updates chart viewport when axisVisibleMin/Max change
+  NumericAxisController? axisController;
+  // Used to determine where the user tapped to begin swiping.
+  int? selectedPointIndex;
+  // Used to determine if a tap/swap was programmatic or manual.
+  // Tap provides pointIndex, swipe provides viewportPointIndex
+  bool manualTap = false;
+
+  @override
+  void initState() {
+    //Initialize the data source to the chart
     final dataPoints = <EcgDataPoint>[];
-    final startMs = startTime.millisecondsSinceEpoch.toDouble();
-    for (var row in ecgRows) {
+    final startMs = widget.startTime.millisecondsSinceEpoch.toDouble();
+    for (var row in widget.ecgRows) {
       final timestampMs = (row['timestamp_ms'] as int).toDouble() + startMs;
       final samples = List<int>.from(row['ecg_data']);
       for (int i = 0; i < samples.length; i++) {
@@ -27,6 +47,19 @@ class HistoricalChart extends StatelessWidget {
         );
       }
     }
+    // Decided to assign instead of using late to possibly optimize
+    // for querying in chunks.
+    chartData = dataPoints;
+    // Screen will maintain a 10 second window
+    axisVisibleMin = dataPoints.first.ecgTime;
+    axisVisibleMax = dataPoints.first.ecgTime + 10000;
+    // Enabling selection behavior
+    selectionBehavior = SelectionBehavior(enable: true);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: SizedBox.expand(
         child: SfCartesianChart(
@@ -46,10 +79,11 @@ class HistoricalChart extends StatelessWidget {
                 const TextStyle(color: Colors.white),
               );
             },
-            //
-            initialVisibleMinimum: dataPoints.first.ecgTime,
-            initialVisibleMaximum:
-                dataPoints.first.ecgTime + 10000, // 10 seconds window
+            onRendererCreated: (NumericAxisController controller) =>
+                axisController = controller,
+
+            initialVisibleMinimum: axisVisibleMin,
+            initialVisibleMaximum: axisVisibleMax,
           ),
           primaryYAxis: NumericAxis(
             minimum: 0,
@@ -64,9 +98,10 @@ class HistoricalChart extends StatelessWidget {
             enablePanning: true,
             enableDoubleTapZooming: true,
           ),
+
           series: [
             LineSeries<EcgDataPoint, double>(
-              dataSource: dataPoints,
+              dataSource: chartData,
               xValueMapper: (p, _) => p.ecgTime,
               yValueMapper: (p, _) => p.ecgValue,
               color: const Color.fromARGB(255, 228, 10, 10),
@@ -74,8 +109,70 @@ class HistoricalChart extends StatelessWidget {
               name: "Ecg value",
             ),
           ],
+          // Important. Sliding starts with selecting a point
+          // So all logic is dependant on it.
+          onSelectionChanged: (SelectionArgs args) => updateSelectedPoint(args),
+          // Important. After the initial swiping motion begins and we act on it.
+          // Thus using the updated axisVisibleMin/Max and direction we  swipe.
+          onPlotAreaSwipe: (ChartSwipeDirection direction) =>
+              performSwipe(direction),
         ),
       ),
     );
+  }
+
+  /// When the user taps or swipes they select a point internally.
+  /// Updates axisVisibleMin + Max to adjust bounds of x-axis viewport.
+  void updateSelectedPoint(SelectionArgs args) {
+    // While manually selecting the points.
+    if (!manualTap) {
+      selectedPointIndex = args.viewportPointIndex;
+    }
+    // While swiping the point gets selected.
+    else {
+      selectedPointIndex = args.pointIndex;
+    }
+
+    // Sets visible minimum and visible maximum to maintain
+    // the selected point in the center of viewport
+    axisVisibleMin = selectedPointIndex! - 2.toDouble();
+    axisVisibleMax = selectedPointIndex! + 2.toDouble();
+  }
+
+  /// Updates the axisController based on the axisVisibleMin / Max
+  /// Which were just updated in updateSelectedPoints
+  void performSwipe(ChartSwipeDirection direction) {
+    // Executes when swiping the chart from right to left
+    if (direction == ChartSwipeDirection.end &&
+        (axisVisibleMax + 20.toDouble()) < chartData.length) {
+      manualTap = true;
+      // Set the visible minimum and visible maximum to maintain
+      // The selected point in the center of the viewport.
+      axisVisibleMin = axisVisibleMin + 20.toDouble();
+      axisVisibleMax = axisVisibleMax + 20.toDouble();
+      // To update the visible maximum and visible minimum dynamically by using axis controller.
+      axisController!.visibleMinimum = axisVisibleMin;
+      axisController!.visibleMaximum = axisVisibleMax;
+      // To execute after chart redrawn with new visible minimum and maximum,
+      // We used delay for 1 second to give the chart time to render the changes.
+      Future.delayed(const Duration(milliseconds: 1000), () {
+        // Public method used to select the data point dynamically
+        selectionBehavior.selectDataPoints((axisVisibleMin.toInt()) + 2);
+      });
+    }
+    // Executes when swiping the chart from left to right
+    // Same logic as above
+    else if (direction == ChartSwipeDirection.start &&
+        (axisVisibleMin - 20.toDouble()) >= 0) {
+      setState(() {
+        axisVisibleMin = axisVisibleMin - 20.toDouble();
+        axisVisibleMax = axisVisibleMax - 20.toDouble();
+        axisController!.visibleMinimum = axisVisibleMin;
+        axisController!.visibleMaximum = axisVisibleMax;
+        Future.delayed(const Duration(milliseconds: 20), () {
+          selectionBehavior.selectDataPoints((axisVisibleMin.toInt()) + 2);
+        });
+      });
+    }
   }
 }
